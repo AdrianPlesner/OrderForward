30176c7ec835bf144d075bee7525bde5ee3d4644
Adrian Plesner <adrian.plesner@gmail.com>
2025-02-26 21:19:13 +0100

    self update done

--- update_handler.py
+++ update_handler.py
@@ -1,10 +1,13 @@
 import os
+import shutil
+import subprocess
+import tempfile
 import tkinter as tk
 import urllib.request
 
 import requests
 
-CURRENT_VERSION = "1.2"
+CURRENT_VERSION = "1.1"
 GITHUB_RELEASE_URL = "https://api.github.com/repos/AdrianPlesner/OrderForward/releases/latest"
 
 class UpdateHandler(tk.Frame):
@@ -15,6 +18,7 @@ class UpdateHandler(tk.Frame):
         self.get_latest_version_number()
         self.label = tk.Label(self, text=f'latest tag: {self.latest_tag}')
         self.label.pack()
+        self.want_update = False
 
     def get_latest_version_number(self):
         r = requests.get(GITHUB_RELEASE_URL)
@@ -25,13 +29,24 @@ class UpdateHandler(tk.Frame):
         return float(self.latest_tag) > float(CURRENT_VERSION)
 
     def install_update(self):
-        exe_dir = os.path.join(os.getcwd(),"OrderForward.exe")
+        tmp_dir = tempfile.gettempdir()
+        update_exe = "OrderForwardUpdater.exe"
+        dest_file = os.path.join(tmp_dir, update_exe)
+        src_file = os.path.abspath(os.path.join(os.path.dirname(__file__), update_exe))
+        shutil.copy(src_file, dest_file)
+
+
+        tmp_exe_dir = os.path.join(tmp_dir,"OrderForward.exe")
+        running_exe_dir = os.path.join(os.getcwd(), "OrderForward.exe")
         r = requests.get(GITHUB_RELEASE_URL)
         if r.status_code == 200:
             file_url = r.json()['assets'][0]['browser_download_url']
-            os.replace()
-            urllib.request.urlretrieve(file_url, exe_dir)
+            print("Downloading update")
+            urllib.request.urlretrieve(file_url, tmp_exe_dir)
+            subprocess.Popen([dest_file, str(os.getpid()), running_exe_dir, tmp_exe_dir], shell=False)
+            print("closing window")
             self.master.destroy()
+
 
     def open_dialog(self):
         d = tk.Toplevel(self.master)
@@ -46,5 +61,4 @@ class UpdateHandler(tk.Frame):
         label.pack()
         buttons_frame.pack()
         d.attributes("-topmost", True)
-
 
--- updater.py
+++ updater.py
@@ -1,12 +1,29 @@
 import os
+import signal
+import subprocess
 import sys
+from time import localtime, strftime, time
 
 
 def main():
-    old_exe = sys.argv[1]
-    new_exe = sys.argv[2]
-    os.remove(old_exe)
-    os.replace(new_exe, old_exe)
+    parent_pid = int(sys.argv[1])
+    print(f"killing {parent_pid}")
+    try:
+        os.kill(parent_pid, signal.SIGTERM)
+    except Exception as e:
+        print("Parent process already terminated")
+    old_exe = sys.argv[2]
+    new_exe = sys.argv[3]
+    print("replacing file")
+    try:
+        os.remove(old_exe)
+        os.replace(new_exe, old_exe)
+        print("file replaced")
+        subprocess.Popen(old_exe, shell=True)
+    except Exception as e:
+        print(f"An error occurred while updating: {e}")
+        with open(os.path.join(os.getcwd(),"errorLog" + str(strftime("%Y/%B/%D-%H:%M:%S", localtime(time()))) + ".txt"), 'w') as f:
+            f.write(f'{e}')
 
 
 if __name__ == "__main__":
